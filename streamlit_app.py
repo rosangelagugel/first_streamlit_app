c61595b95d09f8da03400e60cfb020077c45beac
Rosangela Gugel <37376499+rosangelagugel@users.noreply.github.com>
2022-10-05 23:21:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,22 +29,18 @@ def get_fruityvice_data(fruit_choice):
 
 #New Section to display fruiyvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+back_from_function = get_fruityvice_data(fruit_choice)
+streamlit.dataframe(back_from_function)
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-	 my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
+	my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 # Add a button to load the fruit
-if streamlit.button('Get Fruito Load List'):
+if streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)   
