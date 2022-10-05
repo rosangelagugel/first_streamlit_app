19929715481bf394965a564104988ae8822ad9f4
Rosangela Gugel <37376499+rosangelagugel@users.noreply.github.com>
2022-10-05 22:27:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,6 @@ try:
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
    else:
-       streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+
